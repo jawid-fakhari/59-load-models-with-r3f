1190d1e20060674520278586eba89c9d10ee61ad
jawid-fakhari <jawid_fakhari@live.it>
2024-10-11 15:29:23 +0200

    Loading a model

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,31 +1,30 @@
 import { OrbitControls } from '@react-three/drei'
+import { useLoader } from '@react-three/fiber'
 import { Perf } from 'r3f-perf'
+import { GLTFLoader } from 'three/examples/jsm/Addons.js'
 
-export default function Experience()
-{
+
+export default function Experience() {
+    const model = useLoader(GLTFLoader, './hamburger.glb')
+
     return <>
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-        <ambientLight intensity={ 1.5 } />
+        <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
+        <ambientLight intensity={1.5} />
 
-        <mesh castShadow position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
 
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
 
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+        <mesh receiveShadow position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
+
+        {/* con primitive posso inserire il mio modello alla scena */}
+        <primitive object={model.scene} scale={0.35} />
 
     </>
 }
