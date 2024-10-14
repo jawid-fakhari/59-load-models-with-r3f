836ba4afd7b7bb09f51b9c5e3f43b50fb54a691a
jawid-fakhari <jawid_fakhari@live.it>
2024-10-14 15:03:16 +0200

    Leva debugger

--- src/Fox.jsx
+++ src/Fox.jsx
@@ -1,25 +1,32 @@
 import { useAnimations, useGLTF } from '@react-three/drei'
+import { useControls } from 'leva';
 import { useEffect } from 'react';
 
 export default function Fox() {
     const fox = useGLTF('./Fox/glTF/Fox.gltf')
     const animations = useAnimations(fox.animations, fox.scene);
+    // console.log(animations.names)
+
+    const { animationName } = useControls({
+        animationName: { options: animations.names }
+    })
 
     useEffect(() => {
-        const action = animations.actions.Run
-        action.play()
+        const action = animations.actions[animationName]
+        action
+            .reset()
+            .fadeIn(0.5)
+            .play()
 
-        //Animation crossFadeFrom after 2 seconds
-        window.setTimeout(() => {
-            animations.actions.Walk.play()
-            animations.actions.Walk.crossFadeFrom(animations.actions.Run, 1)
-        }, 2000)
-    }, [])
+        return () => {
+            action.fadeOut(0.5)
+        }
+    }, [animationName])
 
     return <>
         <primitive
             object={fox.scene}
-            scale={0.05}
+            scale={0.02}
             position={[-2.5, 0, 2.5]}
         />
     </>
